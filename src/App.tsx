209fe0de3b48ba0b3ed94ab6c854746603fd7380
Shane Padgett <contact@shanepadgett.dev>
2021-10-22 23:49:45 -0500

    fixed extensions and formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import EvenNumberFilter from './components/array/filter/examples/EvenNumFilter';
+import ObjectFilter from './components/array/filter/examples/ObjectFilter';
+import InlineArrayFilter from './components/array/filter/examples/InlineArrayFilter';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <EvenNumberFilter />
+      <InlineArrayFilter />
+      <ObjectFilter />
+    </>
+  );
 }
 
 export default App;
